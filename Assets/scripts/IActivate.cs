63a14070327eb5970a20da9fbf9c03e419e2270e
Home_PC\Julio <jachana@me.com>
2021-10-26 23:55:46 +0200

    Activate interface

---
+++ Assets/scripts/IActivate.cs
@@ -0,0 +1,9 @@
+public interface IActivate
+{
+    void Activate();
+    void Deactivate();
+    void Enhance();
+    void Diminish();
+    void Toggle();
+    void ActivateForSeconds(float time_t);
+}
